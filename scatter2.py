f94260792a75a32f3641d5934be8762bf5cf1cf1
Phanindra <phanindra.nitw@gmail.com>
2019-11-01 14:34:54 -0500

    scatter matrices maximizing between-class

---
+++ scatter2.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import sys
+
+def scatter (k, dataPath, labelsPath, vectorPath, reducedDataPath):
+  X = np.genfromtxt(dataPath, delimiter=',')
+  Y = np.genfromtxt(labelsPath)
+  df = pd.DataFrame(X)
+  X_mean = df.mean(axis=0)
+  X_gmean = df.groupby(Y, axis=0).mean()
+
+  M = df.groupby(Y, axis=0).count().to_numpy()[:,0]
+
+  mean_diff = X_gmean - X_mean
+  mean_diff_count = mean_diff.mul(M, axis=0)
+
+  B = np.matmul(mean_diff_count.to_numpy().transpose(), mean_diff.to_numpy())
+
+  # gives eigenvalues in asc order
+  evals, evecs = np.linalg.eigh(B)
+  print(evecs)
+  # sort in reverse order
+  idx = np.argsort(evals)[::-1]
+  evals = evals[idx]
+  evecs = evecs[:,idx]
+
+  # get first k eigenvectors (least dominant) and set the rest of the columns to zero
+  evecs[:,k:] = 0
+  B_max = np.matmul(B, evecs)
+
+  np.savetxt(vectorPath, evecs[:, :k], delimiter=',')
+  np.savetxt(reducedDataPath, B_max[:, :k].transpose(), delimiter=',')
+  return
+
+if __name__ == "__main__":
+  dataPath = 'iris.data'
+  labelsPath = 'iris.labels'
+  vectorPath = 'scatter2_vectors.csv'
+  reducedDataPath = 'scatter2_reduced_data.csv'
+  if False and len(sys.argv) < 5:
+    print ('Please check parameters and try again.')
+    print ('eg: python [program1] [input_data] [input_labels] [output_vector] [output_reduced_data]')
+  else:
+    dataPath = sys.argv[1] if len(sys.argv) > 1 else dataPath
+    labelsPath = sys.argv[2] if len(sys.argv) > 2 else labelsPath
+    vectorPath = sys.argv[3] if len(sys.argv) > 3 else vectorPath
+    reducedDataPath = sys.argv[4] if len(sys.argv) > 4 else reducedDataPath
+    k = 2
+    scatter(k, dataPath, labelsPath, vectorPath, reducedDataPath)
